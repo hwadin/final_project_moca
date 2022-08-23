61b5d845c01e3f15a8a869090c50b8aa1d08c1a1
hwadin <shj1478@naver.com>
2022-08-23 18:16:45 +0900

    ㅎㅎ

--- kte_final_moca/src/main/java/net/koreate/moca/cafe/controller/CafeController.java
+++ kte_final_moca/src/main/java/net/koreate/moca/cafe/controller/CafeController.java
@@ -3,13 +3,19 @@ package net.koreate.moca.cafe.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import lombok.RequiredArgsConstructor;
+import net.koreate.common.utils.SearchCriteria;
+import net.koreate.moca.cafe.service.CafeService;
 
 
 @Controller
 @RequestMapping("/cafe/*")
-
+@RequiredArgsConstructor
 public class CafeController {
 
+	private final CafeService cs;
 
 	@GetMapping("cafeMain")
 	public String CafeMain() {
@@ -21,6 +27,7 @@ public class CafeController {
 		return "cafe/cafeDetail";
 	}
 	
+
 
 	
 }
