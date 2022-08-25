84a4ca15a686d0d7d29b1db9863865301a97f64d
SinWoo <raltlsdn@naver.com>
2022-08-25 20:35:01 +0900

    header search compl

--- kte_final_moca/src/main/java/net/koreate/moca/cafe/service/CafeService.java
+++ kte_final_moca/src/main/java/net/koreate/moca/cafe/service/CafeService.java
@@ -2,28 +2,28 @@ package net.koreate.moca.cafe.service;
 
 import java.util.List;
 
-import net.koreate.common.utils.Criteria;
 import net.koreate.moca.cafe.vo.CafeVO;
 
 public interface CafeService {
 
 	// 카페 정보 등록
 	void regist(CafeVO vo) throws Exception;
-	
-	// 카페 상세 정보 읽어오기 (카페 번호로 조회)  
+
+	// 카페 상세 정보 읽어오기 (카페 번호로 조회)
 	CafeVO read(int no) throws Exception;
-	
-	// 카페 리스트 읽어오기 
+
+	// 카페 리스트 읽어오기
 	List<CafeVO> cafeList(int index) throws Exception;
-	
+
 	// 카페 정보 수정
 	void update(CafeVO vo) throws Exception;
 
 	// 카페 좋아요 수 업데이트
 	void updatelikenum(int no) throws Exception;
-	
+
 	// 카페 정보 삭제(카페 번호로 조회)
-	void delete(int no) throws Exception; 
-	
+	void delete(int no) throws Exception;
+
+	List<CafeVO> findByKeyword(String keyword);
 
 }
